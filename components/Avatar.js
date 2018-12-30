61c0566966c0db4a30b547f4975193506c8b1631
daniel <dotexe0@gmail.com>
2018-12-30 15:56:18 -0800

    simple avatar component with style passed in as props

---
+++ components/Avatar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ColorPropType, StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default function Avatar({ size, backgroundColor, initials })({
+    const style = {
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        backgroundColor
+    }
+    return <View style={style} />
+})
+
+Avatar.propTypes = {
+    initials: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired,
+    backgroundColor: ColorPropType.isRequired
+}
