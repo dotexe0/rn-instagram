611caba88a8120d72dcdc52bc3c6d78599d833fa
daniel <dotexe0@gmail.com>
2018-12-30 17:26:35 -0800

    Avatar rendering in top left

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import { Constants } from 'expo'
 import { StyleSheet, Text, View } from 'react-native';
+import Avatar from './components/Avatar';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Avatar size={35} backgroundColor={'teal'} initials={'dv'} />
       </View>
     );
   }
@@ -15,9 +15,10 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
 });
--- components/Avatar.js
+++ components/Avatar.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { ColorPropType, StyleSheet, Text, View } from 'react-native';
-import PropTypes from 'prop-types';
+import React from "react";
+import { ColorPropType, StyleSheet, Text, View } from "react-native";
+import PropTypes from "prop-types";
 
-export default function Avatar({ size, backgroundColor, initials })({
-    const style = {
-        width: size,
-        height: size,
-        borderRadius: size / 2,
-        backgroundColor
-    }
-    return <View style={style} />
-})
+export default function Avatar({ size, backgroundColor, initials }) {
+  const style = {
+    width: size,
+    height: size,
+    borderRadius: size / 2,
+    backgroundColor
+  };
+  return (
+    <View style={[styles.container, style]}>
+      <Text style={styles.text}>{initials}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    color: "white"
+  }
+});
 
 Avatar.propTypes = {
-    initials: PropTypes.string.isRequired,
-    size: PropTypes.number.isRequired,
-    backgroundColor: ColorPropType.isRequired
-}
+  initials: PropTypes.string.isRequired,
+  size: PropTypes.number.isRequired,
+  backgroundColor: ColorPropType.isRequired
+};
